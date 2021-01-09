fd97c12c5c4f1c07008f35eae0856a93d7365e17
Jan <honzicekcahlicek1@gmail.com>
2021-01-09 23:38:31 +0100

    Users details init

--- src/views/Home/homeRoutes.js
+++ src/views/Home/homeRoutes.js
@@ -1,4 +1,5 @@
 import Users from "./Users.svelte"
+import User from "./User.svelte";
 import Communities from "./Communities.svelte"
 import Tests from "./Tests.svelte"
 import TestDetail from "./TestDetail.svelte"
@@ -6,6 +7,7 @@ import UsersStatistics from "./UsersStatistics.svelte"
 
 export let routes = {
     "/users": Users,
+    "/user/:id": User,
     "/communities": Communities,
     "/tests":Tests,
     "/tests/:id": TestDetail,
