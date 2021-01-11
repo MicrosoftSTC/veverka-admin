5aa05aedc02135e1338dd472b5376101f46cd0ad
Jan <honzicekcahlicek1@gmail.com>
2021-01-11 20:02:46 +0100

    user details user field card

--- src/views/Home/UserDetail.svelte
+++ src/views/Home/UserDetail.svelte
@@ -1,6 +1,11 @@
 <script>
     import {mockedUsers} from "../../stores/mockUsers";
 
+    import Card from 'svelte-materialify/src/components/Card/Card.svelte';
+    import Col from 'svelte-materialify/src/components/Grid/Col.svelte';
+    import Row from 'svelte-materialify/src/components/Grid/Row.svelte';
+    import Avatar from 'svelte-materialify/src/components/Avatar/Avatar.svelte'
+
     export let params = {};
     let selectedUser;
 
@@ -8,4 +13,36 @@
         selectedUser = u.filter(user => user.id == params.id)[0]
     })
 </script>
-<p>users details page</p>
+<h3 class="text-h4">User detail</h3>
+<Card outlined style="min-width: 100%" class="mt-3">
+    <div class="pl-4 pr-4 pt-3">
+        {#if selectedUser}
+            <Row>
+                <Col>
+                    <div>
+                        <div style="margin: auto; text-align: center">
+                            <Avatar size={150}><img src="{`//picsum.photos/${selectedUser.id * Math.random().toString().slice(2, 5)}`}"/></Avatar>
+                        </div>
+                        <div class="text-h6 mt-3 mb-3" style="text-align: center">{selectedUser.username}</div>
+                    </div>
+                </Col>
+                <Col>
+                    <div class="mt-3">
+                        {#each Object.keys(selectedUser) as field}
+                            <div class="d-flex justify-space-between mr-16">
+                                <p class="text-subtitle-2">{field.toString().slice(0, 1).toUpperCase() + field.toString().slice(1, field.length)}</p>
+                                {#if field === "joined"}
+                                    <p>{selectedUser[field].toString().slice(0, 15)}</p>
+                                {:else if field === "completedUsers" || field === "questions" || field === "reports"}
+                                    <p>{selectedUser[field].length}</p>
+                                {:else}
+                                    <p>{selectedUser[field]}</p>
+                                {/if}
+                            </div>
+                        {/each}
+                    </div>
+                </Col>
+            </Row>
+        {/if}
+    </div>
+</Card>
